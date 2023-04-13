ba49a463f8c9f9543a36950ff4a298cd81be1ea3
nhungwt <nhunghongwt@gmail.com>
2023-04-13 22:23:46 +0700

    Bottom navigation

---
+++ app/src/main/java/com/example/th_4_13/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.th_4_13;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
+    private ActionBar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+        toolbar = getSupportActionBar();
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.navigation_list:
+                        toolbar.setTitle("List");
+                        return true;
+                    default:
+                        toolbar.setTitle("Def");
+                        return true;
+                }
+            }
+        });
+    }
+}
